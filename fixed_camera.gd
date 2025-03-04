70a6f40d27184d4fbac9b71f7f4015dbb7b10b9c
Vedant Singhal <vedantsinghal07@gmail.com>
2025-03-04 12:20:55 +0530

    remove unused files

--- fixed_camera.gd
+++ fixed_camera.gd
@@ -1,11 +1,12 @@
 extends Camera3D
 
 @export var target_path: NodePath
-@export var offset := Vector3(10, 5, 10)  # x = side offset, y = height, z = distance
+@export var offset := Vector3(30, 5, 0)  # x = side offset, y = height, z = distance
 @export var view_type := "top"  # top, left, right, front
 @export var ortho_size := 7  # Controls the zoom level for orthographic cameras
 
 var target: Node3D
+var orientation_node: Node3D
 
 func _ready():
 	print("Camera ready")
@@ -18,11 +19,12 @@ func configure_camera(new_view_type: String, new_target: Node3D):
 	print("Configuring camera: ", new_view_type)
 	view_type = new_view_type
 	target = new_target
+	orientation_node = target.get_child(1)  # Assuming the player has a child node for orientation
 	projection = Camera3D.PROJECTION_ORTHOGONAL
 	size = ortho_size  # This controls how much is visible
 	print("Camera configured with view type: ", view_type, " Target set: ", target != null)
 
-func _process(_delta):
+func _physics_process(delta):
 	if target:
 		var target_transform = target.global_transform
 		
@@ -30,7 +32,7 @@ func _process(_delta):
 			"left":
 				# Position to the left of the player based on player's orientation
 				# Calculate position in a circle around the player
-				var angle = -target.get_child(1).rotation.y
+				var angle = -orientation_node.rotation.y
 				var pos_x = target.global_position.x + cos(angle) * offset.x
 				var pos_z = target.global_position.z + sin(angle) * offset.x
 				global_position = Vector3(pos_x, target.global_position.y + offset.y/3, pos_z)
@@ -40,10 +42,10 @@ func _process(_delta):
 			"right":
 				# Position to the right of the player based on player's orientation
 				# Calculate position in a circle around the player
-				var angle = -target.get_child(1).rotation.y -PI
+				var angle = -orientation_node.rotation.y -PI
 				var pos_x = target.global_position.x + cos(angle) * offset.x
 				var pos_z = target.global_position.z + sin(angle) * offset.x
-				global_position = Vector3(pos_x, target.global_position.y + offset.y/3, pos_z)
+				global_position = Vector3(pos_x, orientation_node.global_position.y + offset.y/3, pos_z)
 				# Look at player from the right
 				look_at(target.global_position, Vector3.UP)
 				
